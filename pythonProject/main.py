ba83f2a24abf7335d0880da9d720f137cacbb566
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2024-02-27 11:36:49 +0530

    Completed Pomodoro app file

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -10,6 +10,8 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
@@ -18,15 +20,46 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def start_count():
-    countdown(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        countdown(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+
+def reset():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(canvas_text, text="00:00")
 
 
 def countdown(count):
-    minutes = math.floor(count/60)
+    minutes = math.floor(count / 60)
     seconds = count % 60
+    if seconds < 10:
+        seconds = f"0{seconds}"
+
     canvas.itemconfig(canvas_text, text=f"{minutes}:{seconds}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_count()
+        reps_check = math.floor(reps / 2) * "✔"
+        checkmark_label.config(text=reps_check)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,11 +81,11 @@ start_button = Button(text="Start", command=start_count)
 start_button.pack(side="left")
 start_button.config(pady=3, padx=10)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset)
 reset_button.pack(side="right")
 reset_button.config(pady=3, padx=10)
 
-checkmark_label = Label(text="✔")
+checkmark_label = Label()
 checkmark_label.pack(side="bottom")
 
 window.mainloop()
